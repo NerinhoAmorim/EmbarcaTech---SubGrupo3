e1f52297e3520526b75608ef3b58d5bd82ff4d65
LucasEmmanuelRM <lucas.emmanuel10@hotmail.com>
2024-12-24 12:19:03 -0300

    Adição do conversor de temperatura

---
+++ Conversor-Unidades-C/src/include/temperatura.h
@@ -0,0 +1,16 @@
+#ifndef temperatura_h
+#define temperatura_h
+
+// Funções de conversão
+float igual(float value);
+float celsiusFahrenheit(float c);
+float celsiusKelvin(float c);
+float fahrenheitCelsius(float f);
+float fahrenheitKelvin(float f);
+float kelvinCelsius(float k);
+float kelvinFahrenheit(float k);
+
+// Função principal para executar o conversor
+void conversorDeTemperatura();
+
+#endif
