53ae22f3182dd90a3718fd83cef8fec4453062ae
ac1oly <guacioly@gmail.com>
2024-12-26 14:07:55 -0300

    Codigo da funcao para converter a velocidade. km/h, m/s e mph. Codigo do cabecalho da classe velocidade.

---
+++ Conversor-Unidades-C/src/velocidade.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+struct Velocidade {
+    int tipo;
+    float valor;
+    int tipo2;
+};
+
+struct Velocidade CadastrarVelocidade() {
+    struct Velocidade v;
+
+    printf("Digite o valor: ");
+    scanf("%f", &v.valor);
+
+    printf("Escolha o tipo de medida:\n");
+    printf("1 - km/h  2 - m/s  3 - mph\n");
+    scanf("%d", &v.tipo);
+
+    printf("Para qual medida você vai querer converter:\n");
+    printf("1 - km/h  2 - m/s  3 - mph\n");
+    scanf("%d", &v.tipo2);
+
+    return v;
+}
+
+void ConverterVelocidade(struct Velocidade v) {
+    float conver;
+
+    if (v.tipo == 1 && v.tipo2 == 1) {
+        conver = v.valor;
+        printf("%.2f km/h\n", conver);
+    } else if (v.tipo == 1 && v.tipo2 == 2) {
+        conver = v.valor / 3.6;
+        printf("%.2f m/s\n", conver);
+    } else if (v.tipo == 1 && v.tipo2 == 3) {
+        conver = v.valor / 1.609;
+        printf("%.2f mph\n", conver);
+    } else if (v.tipo == 2 && v.tipo2 == 1) {
+        conver = v.valor * 3.6;
+        printf("%.2f km/h\n", conver);
+    } else if (v.tipo == 2 && v.tipo2 == 2) {
+        conver = v.valor;
+        printf("%.2f m/s\n", conver);
+    } else if (v.tipo == 2 && v.tipo2 == 3) {
+        conver = v.valor * 2.237;
+        printf("%.2f mph\n", conver);
+    } else if (v.tipo == 3 && v.tipo2 == 1) {
+        conver = v.valor * 1.609;
+        printf("%.2f km/h\n", conver);
+    } else if (v.tipo == 3 && v.tipo2 == 2) {
+        conver = v.valor / 2.237;
+        printf("%.2f m/s\n", conver);
+    } else {
+        conver = v.valor;
+        printf("%.2f mph\n", conver);
+    }
+}
