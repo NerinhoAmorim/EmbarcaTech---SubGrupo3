feb3dd89c7360dd4199aaf9c5a86736c76a61b45
GuiPD1610 <156451203+GuiPD1610@users.noreply.github.com>
2024-12-26 22:41:38 -0300

    Atualizaçao2

--- Conversor-Unidades-C/src/include/dados.h
+++ Conversor-Unidades-C/src/include/dados.h
@@ -1,56 +1,25 @@
-#ifndef DADOS_H
-#define DADOS_H
+#ifndef DIGITAL_H
+#define DIGITAL_H
 
-#define B_KBC 1024.0        
-#define B_MBC 1048576.0      
-#define B_GBC 1073741824.0
+#define B_KBC 1024.0     
+#define B_MBC 1048576.0     
+#define B_GBC 1073741824.0   
+double b_kb(double byte);
+double b_mb(double byte);
+double b_gb(double byte);
 
-double b_kb(double byte) {
-    return byte / B_KBC;
-}
+double kb_b(double kilo_byte);
+double kb_mb(double kilo_byte);
+double kb_gb(double kilo_byte);
 
-double b_mb(double byte) {
-    return byte / B_MBC;
-}
+double mb_b(double mega_byte);
+double mb_kb(double mega_byte);
+double mb_gb(double mega_byte);
 
-double b_gb(double byte) {
-    return byte / B_GBC;
-}
+double gb_b(double giga_byte);
+double gb_kb(double giga_byte);
+double gb_mb(double giga_byte);
 
-double kb_b(double kilo_byte) {
-    return kilo_byte * B_KBC;
-}
-
-double kb_mb(double kilo_byte) {
-    return kilo_byte / B_KBC;
-}
-
-double kb_gb(double kilo_byte) {
-    return kilo_byte / B_MBC;
-}
-
-double mb_b(double mega_byte) {
-    return mega_byte * B_MBC;
-}
-
-double mb_kb(double mega_byte) {
-    return mega_byte * B_KBC;
-}
-
-double mb_gb(double mega_byte) {
-    return mega_byte / B_KBC;
-}
-
-double gb_b(double giga_byte) {
-    return giga_byte * B_GBC;
-}
-
-double gb_kb(double giga_byte) {
-    return giga_byte * B_MBC;
-}
-
-double gb_mb(double giga_byte) {
-    return giga_byte * B_KBC;
-}
+void executar_digital();
 
 #endif
